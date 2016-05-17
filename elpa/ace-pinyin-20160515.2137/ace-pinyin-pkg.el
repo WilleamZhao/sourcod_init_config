d2e00d9c362030676fccf5ad3bccbe805c82c6b0
willeamzhao <zhaochunjie@sourcod.com>
2016-05-17 10:28:49 +0800

    delete elpa

--- elpa/ace-pinyin-20160515.2137/ace-pinyin-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ace-pinyin" "20160515.2137" "Jump to Chinese characters using ace-jump-mode or avy" '((ace-jump-mode "2.0") (avy "0.2.0") (pinyinlib "0.1.0")) :url "https://github.com/cute-jumper/ace-pinyin" :keywords '("extensions"))
--- elpa/popup-20160409.2133/popup-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("popup.el") (22329 13742 610336 762000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
